bb30fc07654985df98d92b44c6561e75ab54e2fa
Ray Mallick <rmallick6806@gmail.com>
2015-06-21 18:43:17 -0400

    Fixed dropbox setup

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,9 +1,13 @@
 class Listing < ActiveRecord::Base
+	if Rails.env.development?
+		has_attached_file :itemimage, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "raymallick.png"
+	else 	
 	has_attached_file :itemimage, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "raymallick.png",
 						:storage => :dropbox,
    						:dropbox_credentials => Rails.root.join("config/dropbox.yml")
-
+   						:path => ":style/:id_filename"
 	validates_attachment_content_type :itemimage, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
+	end
 end
 	
