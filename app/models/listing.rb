1b4020568210a49401fd5be9cbd75bdec169c8e1
Ray Mallick <rmallick6806@gmail.com>
2015-06-21 18:47:44 -0400

    Fixed dropbox save path

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,7 +5,7 @@ class Listing < ActiveRecord::Base
 	has_attached_file :itemimage, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "raymallick.png",
 						:storage => :dropbox,
    						:dropbox_credentials => Rails.root.join("config/dropbox.yml")
-   						:path => ":style/:id_filename"
+   						:path => ":style/:id_:filename"
 	validates_attachment_content_type :itemimage, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
 	end
