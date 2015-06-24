95c0ebaf4425b2edfb670ebc97180f048c579168
Ray Mallick <rmallick6806@gmail.com>
2015-06-23 20:09:58 -0400

    Added user support

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'uglifier', '>= 1.3.0'
 gem "paperclip", "~> 4.2"
 gem "paperclip-dropbox", ">= 1.1.7"
 gem "figaro"
+gem "devise"
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.1.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,8 +4,9 @@ class Listing < ActiveRecord::Base
 	else 	
 	has_attached_file :itemimage, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "raymallick.png",
 						:storage => :dropbox,
-   						:dropbox_credentials => Rails.root.join("config/dropbox.yml")
+   						:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
    						:path => ":style/:id_:filename"
+
 	validates_attachment_content_type :itemimage, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
 	end
