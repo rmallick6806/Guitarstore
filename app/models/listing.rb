bca738d2eb178e90d9aefe1f9c9f5fdef807be7b
Ray Mallick <rmallick6806@gmail.com>
2015-06-24 01:01:38 -0400

    Added validations for user and item listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,4 @@
-class Listing < ActiveRecord::Base
+ class Listing < ActiveRecord::Base
 	if Rails.env.development?
 		has_attached_file :itemimage, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "raymallick.png"
 	else 	
@@ -8,7 +8,11 @@ class Listing < ActiveRecord::Base
    						:path => ":style/:id_:filename"
 
 	validates_attachment_content_type :itemimage, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
-
 	end
+
+	validates :name, :descript, :price, presence: true
+	validates :price, numericality: { great_than: 0 }
+	validates_attachment_presence :itemimage, :message => 'is empty. Make sure to attach an image!'
+	
 end
 	
