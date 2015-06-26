a9893f3ed1abd484e3dcc887207daf0908c28aa7
Ray Mallick <rmallick6806@gmail.com>
2015-06-25 23:29:17 -0400

    Set proper visibiity per user

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,5 +1,8 @@
 class ListingsController < ApplicationController
   before_action :set_listing, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!, only: [:new, :create, :edit, :update, :destroy] 
+  before_filter :check_user, only: [:edit, :update, :destroy]
+
 
   # GET /listings
   # GET /listings.json
@@ -73,4 +76,12 @@ class ListingsController < ApplicationController
     def listing_params
       params.require(:listing).permit(:name, :descript, :price, :itemimage)
     end
+    
+    def check_user
+        if current_user != @listing.user
+          redirect_to root_url, alert: "Sorry, this listing doesn't belong to you!"
+        end
+    end
+
+
 end
